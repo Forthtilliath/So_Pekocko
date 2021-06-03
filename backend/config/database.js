e379723c867828c49c030db3d7117b5902b9cbcd
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-03 14:54:28 +0200

    #14.1 - Optimisation db.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 
-exports.connectUser = () => {
+const connect = (id) => {
     mongoose
-        .connect(process.env.DATABASE_USER, { useNewUrlParser: true, useUnifiedTopology: true })
+        .connect(id, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Connexion à MongoDB réussie !'))
         .catch(() => console.log('Connexion à MongoDB échouée !'));
 };
 
-exports.connectAdmin = () => {
-    mongoose
-        .connect(process.env.DATABASE_ADMIN, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('Connexion à MongoDB réussie !'))
-        .catch(() => console.log('Connexion à MongoDB échouée !'));
-};
+exports.connectUser = () => connect(process.env.DATABASE_USER);
+
+exports.connectAdmin = () => connect(process.env.DATABASE_ADMIN);
